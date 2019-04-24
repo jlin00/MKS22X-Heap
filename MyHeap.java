e4c348e337d6001775b2d4d7f2258e204faa6a85
Jackie Lin <jlin00@stuy.edu>
2019-04-24 09:12:10 -0400

    copied driver code and tested heapsort

--- MyHeap.java
+++ MyHeap.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class MyHeap{
   /*
   - size  is the number of elements in the data array.
@@ -74,12 +76,43 @@ public class MyHeap{
       int temp = data[0]; //first element, also the max
       data[0] = data[i]; //swap with last element of heap
       data[i] = temp;
-      pushDown(data, i, 0); //pushdown the top element, size of the heap is equal to index 
+      pushDown(data, i, 0); //pushdown the top element, size of the heap is equal to index
     }
   }
 
-  public static void main(String[] args){
-
-  }
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tmerge /builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          MyHeap.heapsort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+       }
+     }
+     System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+   }
+   System.out.println();
+ }
+}
 
 }
